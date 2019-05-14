1f4f5f2a8026e37510ee72cbf7af526f34fac465
Ashley Lewis <ashleylewis@pursuit.org>
2019-05-14 19:55:04 -0400

    fragments added

--- app/src/main/java/com/example/vetted/MainActivity.java
+++ app/src/main/java/com/example/vetted/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.vetted.FragmentController.Fragmentinterface;
+import com.example.vetted.views.MainFragment;
 
 public class MainActivity extends AppCompatActivity implements Fragmentinterface {
 
@@ -15,6 +16,10 @@ public class MainActivity extends AppCompatActivity implements Fragmentinterface
 
     @Override
     public void showMainFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, MainFragment.newInstance())
+                .addToBackStack(null)
+                .commit();
 
     }
 }
