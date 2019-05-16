576d553d31ec298deff6fbf15b5329dd8f454c70
Ashley Lewis <ashleylewis@pursuit.org>
2019-05-15 22:09:26 -0400

    a

--- app/src/main/java/com/example/vetted/views/DetailsFragment.java
+++ app/src/main/java/com/example/vetted/views/DetailsFragment.java
@@ -36,7 +36,7 @@ public class DetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+g
         return inflater.inflate(R.layout.fragment_details, container, false);
     }
 
