57e3d278563d376b3a28e38216ce39bec07fbc48
plasticspork <plasticspork@Samers-MacBook-Pro-2.local>
2019-05-18 09:35:38 -0400

    recyclerview changes, added collapsing toolbar in detail fragment

---
+++ app/src/main/java/com/example/vetted/controller/RecyclerViewAdapter.java
@@ -0,0 +1,47 @@
+package com.example.vetted.controller;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vetted.R;
+import com.example.vetted.modells.Businesses;
+import com.example.vetted.views.RecyclerViewFragment;
+
+import java.util.List;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewViewholder> {
+    List<Businesses> objects;
+    private RecyclerViewFragment.OnFragmentInteractionListener listener;
+
+    public RecyclerViewAdapter(List<Businesses> objects, RecyclerViewFragment.OnFragmentInteractionListener listener) {
+        this.objects = objects;
+        this.listener = listener;
+    }
+
+    public void setObjects(List<Businesses> objects) {
+        this.objects = objects;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewViewholder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.list_itemview, viewGroup, false);
+        return new RecyclerViewViewholder(view, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewViewholder recyclerViewViewholder, int i) {
+        recyclerViewViewholder.onBind(objects.get(i), listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return objects.size();
+    }
+}
