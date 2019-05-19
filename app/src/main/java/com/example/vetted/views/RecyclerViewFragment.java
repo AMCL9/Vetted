06f6e178a12e7168aa721e0fd3882957101977d9
Samer Mehany <samermehany@pursuit.org>
2019-05-19 15:22:00 -0400

    redone changes to bundles and recyclerview

--- app/src/main/java/com/example/vetted/views/RecyclerViewFragment.java
+++ app/src/main/java/com/example/vetted/views/RecyclerViewFragment.java
@@ -3,7 +3,10 @@ package com.example.vetted.views;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +17,7 @@ import com.example.vetted.controller.RecyclerViewAdapter;
 import com.example.vetted.modells.Businesses;
 import com.example.vetted.network.RetrofitSingleton;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +26,7 @@ import retrofit2.Retrofit;
 
 public class RecyclerViewFragment extends Fragment {
     private RecyclerView recyclerView;
+    private static final String LIST_PARAM = "ListParam";
     private static final String LOGTAG = "TAGTAGTAG";
     private RecyclerViewAdapter adapter;
     private List<Businesses> businessesList = new ArrayList<>();
@@ -32,9 +37,11 @@ public class RecyclerViewFragment extends Fragment {
     }
 
 
-    public static RecyclerViewFragment newInstance() {
+    public static RecyclerViewFragment newInstance(List<Businesses> businessesList) {
         RecyclerViewFragment fragment = new RecyclerViewFragment();
-
+        Bundle args = new Bundle();
+        args.putSerializable(LIST_PARAM, (Serializable) businessesList);
+        fragment.setArguments(args);
         return fragment;
     }
 
@@ -42,7 +49,7 @@ public class RecyclerViewFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-
+            businessesList = (List<Businesses>) getArguments().getSerializable(LIST_PARAM);
         }
         Retrofit retrofit = RetrofitSingleton.getInstance();
     }
@@ -52,6 +59,15 @@ public class RecyclerViewFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_recycler_view, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        adapter = new RecyclerViewAdapter(businessesList, mListener);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(adapter);
     }
 
 
